d78e2fb8940b7b65c430e6202442f9716e27382d
irvan-afk <irvansitumorang7@gmail.com>
2025-02-10 11:11:21 +0700

    menambahkan printf

--- main.c
+++ main.c
@@ -5,7 +5,9 @@ int main(){
   printf("Ini tes dari Fawwaz");
   printf("Masukkan angka pertama");
   scanf("%d",x);
-  printf("Angka pertama telah dimasukkan :)");
+  printf("Angka pertama telah dimasukkan :");
+  printf("aku supri");
+
   printf("tesdulu");
   return 0;
 }

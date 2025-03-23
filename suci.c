6968540090d878bd42861def8ada7ef510d22f40
irvan-afk <irvansitumorang7@gmail.com>
2025-03-23 23:45:40 +0700

    suciBarunihBoss

--- suci.c
+++ suci.c
@@ -26,30 +26,23 @@ void initBG()
     background = LoadTexture("assets/background.png");
 }
 
-// BARU
 Sound clickMenu, selectMenu;
 void InitAssets(Assets *assets)
 {
-    // Muat background yang sudah ada UFO dan teks "space invaders"
     assets->bg = LoadTexture("assets/bgDisplayUtama.png");
 
-    // (Jika memang tidak dipakai lagi, boleh dihapus)
-    // assets->title = LoadTexture("title.png");
-
-    // Muat tombol
     assets->btnPlay = LoadTexture("assets/btnPlay.png");
     assets->btnMenu = LoadTexture("assets/btnMenu.png");
     assets->btnExit = LoadTexture("assets/btnExit.png");
     clickMenu = LoadSound("assets/clickMenu.wav");
     selectMenu = LoadSound("assets/selectMenu.wav");
 
-    assets->menuBackground = LoadTexture("assets/bgMenuScreen.png");  
-    assets->settingsBackground = LoadTexture("assets/bgMenuScreen.png"); 
+    assets->menuBackground = LoadTexture("assets/bgMenuScreen.png");
+    assets->settingsBackground = LoadTexture("assets/bgMenuScreen.png");
     assets->btnOn = LoadTexture("assets/btnOn.png");
     assets->btnOff = LoadTexture("assets/btnOff.png");
     assets->btnBack = LoadTexture("assets/btnBack.png");
     assets->bgMusic = LoadMusicStream("assets/background_music.mp3");
-
 
     PlayMusicStream(assets->bgMusic);
     SetMusicVolume(assets->bgMusic, 0.5f);
@@ -58,7 +51,6 @@ void InitAssets(Assets *assets)
 void UnloadAssetss(Assets *assets)
 {
     UnloadTexture(assets->bg);
-    // UnloadTexture(assets->title); // Jika tidak lagi dipakai
 
     UnloadTexture(assets->btnPlay);
     UnloadTexture(assets->btnMenu);
@@ -70,7 +62,6 @@ void UnloadAssetss(Assets *assets)
 
     UnloadTexture(assets->menuBackground);
     UnloadTexture(assets->settingsBackground);
-
 
     UnloadMusicStream(assets->bgMusic);
     CloseAudioDevice();
@@ -109,14 +100,11 @@ void UpdateMenu(Assets *assets, GameScreen *currentScreen)
         }
     }
 
-    // --- Gambar ---
     BeginDrawing();
     ClearBackground(BLACK);
 
-    // Gambar background
     DrawTexture(assets->bg, 0, 0, WHITE);
 
-    // --- Pengaturan Tombol ---
     float scaleBtn = 2.0f;
     int scaledWidth = (int)(assets->btnPlay.width * scaleBtn);
     int scaledHeight = (int)(assets->btnPlay.height * scaleBtn);
@@ -125,7 +113,6 @@ void UpdateMenu(Assets *assets, GameScreen *currentScreen)
     int buttonX = SCREEN_WIDTH / 2 - scaledWidth / 2;
     int buttonY = 550; // Posisi awal tombol PLAY
 
-    // Tombol PLAY
     if (assets->btnPlay.id > 0)
     {
         Color tintPlay = (menuIndex == MENU_PLAY) ? PURPLE : WHITE;
@@ -168,8 +155,8 @@ void UpdatePlayScreen(GameScreen *currentScreen)
     }
     EndDrawing();
 }
-// Inisialisasi menu pengaturan
-void InitSettingsMenu(SettingsMenu *menu) {
+void InitSettingsMenu(SettingsMenu *menu)
+{
     menu->selectedOption = 0;
     menu->volume = 1.0f;
     menu->screenWidth = SCREEN_WIDTH;
@@ -177,103 +164,116 @@ void InitSettingsMenu(SettingsMenu *menu) {
     menu->options[0] = "On";
     menu->options[1] = "Off";
     menu->options[2] = "Back";
-    }
+}
 
 // Menangani input di menu settings
-void UpdateSettingsMenu(SettingsMenu *menu) {
-    if (IsKeyPressed(KEY_DOWN)) menu->selectedOption = (menu->selectedOption + 1) % 3;
-    if (IsKeyPressed(KEY_UP)) menu->selectedOption = (menu->selectedOption - 1 + 3) % 3;
-    if (menu->selectedOption == 0) {
-        if (IsKeyPressed(KEY_LEFT)) menu->volume = fmax(menu->volume - 0.1f, 0.0f);
-        if (IsKeyPressed(KEY_RIGHT)) menu->volume = fmin(menu->volume + 0.1f, 1.0f);
+void UpdateSettingsMenu(SettingsMenu *menu)
+{
+    if (IsKeyPressed(KEY_DOWN))
+        menu->selectedOption = (menu->selectedOption + 1) % 3;
+    if (IsKeyPressed(KEY_UP))
+        menu->selectedOption = (menu->selectedOption - 1 + 3) % 3;
+    if (menu->selectedOption == 0)
+    {
+        if (IsKeyPressed(KEY_LEFT))
+            menu->volume = fmax(menu->volume - 0.1f, 0.0f);
+        if (IsKeyPressed(KEY_RIGHT))
+            menu->volume = fmin(menu->volume + 0.1f, 1.0f);
     }
-    if (menu->selectedOption == 2 && IsKeyPressed(KEY_ENTER)) {
+    if (menu->selectedOption == 2 && IsKeyPressed(KEY_ENTER))
+    {
         menu->selectedOption = 0;
     }
-    }
-    
-    // // Menampilkan menu settings di layar
-    // void DrawSettingsMenu(SettingsMenu *menu) {
-    // BeginDrawing();
-    // ClearBackground(BLUE);
-    // DrawText("Pengaturan", SCREEN_WIDTH / 2 - 50, 100, 20, RAYWHITE);
-    // for (int i = 0; i < 3; i++) {
-    //     Color color = (i == menu->selectedOption) ? YELLOW : WHITE;
-    //     DrawText(menu->options[i], SCREEN_WIDTH / 2 - 50, 200 + i * 40, 20, color);
-    // }
-    // DrawText(TextFormat("Volume: %.1f", menu->volume), SCREEN_WIDTH / 2 - 50, 240, 20, WHITE);
-    // EndDrawing();
-    // }
+}
 
-void UpdateSettingsScreen(GameScreen *currentScreen, SettingsMenu *menu, Assets *assets) {
+void UpdateSettingsScreen(GameScreen *currentScreen, SettingsMenu *menu, Assets *assets)
+{
     // Navigasi dengan tombol panah
-    if (IsKeyPressed(KEY_DOWN)) {
+    if (IsKeyPressed(KEY_DOWN))
+    {
+        PlaySound(selectMenu);
         menu->selectedOption = (menu->selectedOption + 1) % 3;
     }
-    if (IsKeyPressed(KEY_UP)) {
+    if (IsKeyPressed(KEY_UP))
+    {
+        PlaySound(selectMenu);
         menu->selectedOption = (menu->selectedOption - 1 + 3) % 3;
     }
-    
+
     // Pilihan menu
-    if (IsKeyPressed(KEY_ENTER)) {
-        if (menu->selectedOption == 0) {
+    if (IsKeyPressed(KEY_ENTER))
+    {
+        PlaySound(clickMenu);
+        if (menu->selectedOption == 0)
+        {
             assets->isMusicOn = true;
             PlayMusicStream(assets->bgMusic);
-        } else if (menu->selectedOption == 1) {
+        }
+        else if (menu->selectedOption == 1)
+        {
             assets->isMusicOn = false;
             StopMusicStream(assets->bgMusic);
-        } else if (menu->selectedOption == 2) {
+        }
+        else if (menu->selectedOption == 2)
+        {
             *currentScreen = MENU;
         }
     }
-    
+
     // Gambar tampilan pengaturan
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawTexture(assets->settingsBackground, 0, 0, WHITE);
-    
+
     Color onColor = (menu->selectedOption == 0) ? BLUE : WHITE;
     Color offColor = (menu->selectedOption == 1) ? BLUE : WHITE;
     Color backColor = (menu->selectedOption == 2) ? BLUE : WHITE;
     int centerX = (SCREEN_WIDTH / 2) - ((assets->btnOn.width * 1.5) / 2);
-    
+
     // Posisi tombol ON dan OFF sejajar
     int btnSpacing = 20; // Jarak antara ON dan OFF
     int btnOnX = (SCREEN_WIDTH / 2) - (assets->btnOn.width * 1.5) - (btnSpacing / 2);
     int btnOffX = (SCREEN_WIDTH / 2) + (btnSpacing / 2);
-    
+
     DrawTextureEx(assets->btnOn, (Vector2){btnOnX, 180}, 0.0f, 1.5f, onColor);
     DrawTextureEx(assets->btnOff, (Vector2){btnOffX, 180}, 0.0f, 1.5f, offColor);
-    
-    // Tombol BACK tetap di tengah
-    DrawTextureEx(assets->btnBack, (Vector2){centerX, 240}, 0.0f, 1.5f, backColor);
-    
-    EndDrawing();
-    }
 
-void menuSuci() {
+    DrawTextureEx(assets->btnBack, (Vector2){centerX, 240}, 0.0f, 1.5f, backColor);
+
+    EndDrawing();
+}
+
+void menuSuci()
+{
     Assets assets;
     InitAssets(&assets);
 
     SettingsMenu settingsMenu;
     InitSettingsMenu(&settingsMenu);
-    
+
     GameScreen currentScreen = MENU;
     bool isRunning = true;
-    
-    while (isRunning) {
-        
+
+    while (isRunning)
+    {
+
         UpdateMusicStream(assets.bgMusic);
-        
-        if (currentScreen == MENU) {
+
+        if (currentScreen == MENU)
+        {
             UpdateMenu(&assets, &currentScreen);
-        } else if (currentScreen == PLAY) {
+        }
+        else if (currentScreen == PLAY)
+        {
             UpdatePlayScreen(&currentScreen);
-        } else if (currentScreen == SETTINGS) {
+        }
+        else if (currentScreen == SETTINGS)
+        {
             UpdateSettingsScreen(&currentScreen, &settingsMenu, &assets);
         }
-           if  (currentScreen == EXIT) 
-           {  isRunning = false; // Keluar dari game
+        if (currentScreen == EXIT)
+        {
+            isRunning = false;
         }
     }
 
